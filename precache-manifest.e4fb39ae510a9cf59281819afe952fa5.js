ad50b83b6579eb027e48f638cc0f6df96aeeb6d5
thomvand26 <thomvand26@student.arteveldehs.be>
2020-10-16 20:57:12 +0200

    'deploy'

---
+++ precache-manifest.e4fb39ae510a9cf59281819afe952fa5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2685a9cefd123ff1741f55d5b9d078b2",
+    "url": "/frankbot/index.html"
+  },
+  {
+    "revision": "24d4373780d2b203d38f",
+    "url": "/frankbot/static/css/main.bdc68d56.chunk.css"
+  },
+  {
+    "revision": "b69ef87e14c2865b53e9",
+    "url": "/frankbot/static/js/2.f4cd006f.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/frankbot/static/js/2.f4cd006f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d4373780d2b203d38f",
+    "url": "/frankbot/static/js/main.bedaf50d.chunk.js"
+  },
+  {
+    "revision": "ec8f04f5c0df1db4a3a8",
+    "url": "/frankbot/static/js/runtime-main.ef5e0a7b.js"
+  },
+  {
+    "revision": "68699961c10794ead3b71c1c44d19d1e",
+    "url": "/frankbot/static/media/brain.68699961.rive"
+  },
+  {
+    "revision": "4795c658bd0acf27df64dd67e7d35585",
+    "url": "/frankbot/static/media/frank-deboosere.4795c658.jpg"
+  }
+]);
