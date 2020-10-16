079cbf31cb7759e25f9d2ac3a8e7cce6cd048de8
thomvand26 <thomvand26@student.arteveldehs.be>
2020-10-16 09:01:08 +0200

    'deploy'

---
+++ build/precache-manifest.d5a45748f6a2e70f169967cff466cbeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3342fe32ef1e346c56242e3eb401a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0368f7634908db67581",
+    "url": "/static/css/main.89df7769.chunk.css"
+  },
+  {
+    "revision": "ad463c4fcf78e57db9a8",
+    "url": "/static/js/2.ee1c7a8b.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.ee1c7a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0368f7634908db67581",
+    "url": "/static/js/main.6cc27828.chunk.js"
+  },
+  {
+    "revision": "d40c0afa32b821c13979",
+    "url": "/static/js/runtime-main.5369b4e5.js"
+  },
+  {
+    "revision": "ae14195cacfa6a5b17784ec0b387f140",
+    "url": "/static/media/brain.ae14195c.rive"
+  },
+  {
+    "revision": "4795c658bd0acf27df64dd67e7d35585",
+    "url": "/static/media/frank-deboosere.4795c658.jpg"
+  }
+]);
